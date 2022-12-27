fcada9a5c776d447f2359dc0f7dc12197a66482b
Vova <patutin.vovan@mail.ru>
2022-12-27 15:01:03 +0300

    ready for start in docker

--- service_2/additional/src/main/java/com/example/additional/utils/ConsulRetriever.java
+++ service_2/additional/src/main/java/com/example/additional/utils/ConsulRetriever.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 public class ConsulRetriever {
 
     public List<String> getCrudAddresses() {
-        Consul client = Consul.builder().withUrl("http://localhost:8500").build();
+        Consul client = Consul.builder().withUrl("http://consul:8500").build(); //todo
         AgentClient agentClient = client.agentClient();
         Map<String, Service> services = agentClient.getServices();
         return services.values().stream().filter(x -> x.getTags().contains("basic")).map(service -> service.getAddress() + ":" + service.getPort()).collect(Collectors.toList());
